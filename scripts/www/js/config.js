c32127f8cc7efd588aa389dc1a778621ef05f2fa
musicpatch <280024205@qq.com>
2026-03-20 13:21:38 +0800

    update src file from pakeplus

--- scripts/www/js/config.js
+++ scripts/www/js/config.js
@@ -1,34 +1,37 @@
 const CONFIG={
-    BOARD_SIZE:13,
+    BOARD_SIZE:11,
     CELL_RATIO:0.4,
     COLORS:{
-        BOARD:"#8C9E80",
+        BOARD:"#A5B996",
         BLACK_CHESS:"#333",
         WHITE_CHESS:"#f0f0f0",
         GRID:"#333"
     },
     PLAYER:{
-        BLACK:1, // 先手
-        WHITE:2  // 后手
+        BLACK:1,
+        WHITE:2
     }
 };
-
 const GAME_STATE={
-    currentMode:"traditional", // traditional/skill
+    currentMode:"traditional",
     isPlaying:false,
     cellSize:0,
     boardData:[],
     currentPlayer:CONFIG.PLAYER.BLACK,
-    // 玩家配置
     playerConfig:{
-        1:{type:"human", heroState:null}, // 黑棋：类型+英雄状态
-        2:{type:"human", heroState:null}  // 白棋：类型+英雄状态
+        1:{type:"human", heroState:null},
+        2:{type:"human", heroState:null}
     },
-    // 技能相关临时状态
     skillTemp:{
-        usingSkillPlayer:0, // 正在使用技能的玩家ID
-        skillType:"",       // 当前使用的技能类型
-        selectedHeroIds:[]  // 已选择的英雄ID（用于独占）
+        usingSkillPlayer:0,
+        skillType:"",
+        selectedHeroIds:[],
+        firstX:null,
+        firstY:null,
+        fourInARow:{isExist: false, pos: []},
+        delChess:[],
+        blockSkill:{player:0, remainRound:0},
+        blockArea:[]
     },
     history:[]
 };
